f6716960adae8deb74d3d89e3d3a824eccda73d8
student10 <student10@S7N-PROGLANG-29.pcoip.ki.agh.edu.pl>
2019-02-09 10:10:48 +0100

    implement first test

--- src/main/java/pl/edu/agh/mwo/invoice/product/OtherProduct.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/OtherProduct.java
@@ -2,6 +2,7 @@ package pl.edu.agh.mwo.invoice.product;
 
 import java.math.BigDecimal;
 
+// expected = IllegalArgumentException.class)
 public class OtherProduct extends Product {
 	public OtherProduct(String name, BigDecimal price) {
 		super(name, price, new BigDecimal("0.23"));
